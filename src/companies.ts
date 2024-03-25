74a0eec3f5ba2dd12d51d5bcc2fb4019a831919a
Will Ready <will@readytoflycoaching.com>
2024-03-24 19:26:42 -0700

    Added sponsorship levels

--- src/companies.ts
+++ src/companies.ts
@@ -1,10 +1,24 @@
 import { z } from "zod";
 
 import { statusSchema, visibleBySchema } from "./common";
-import { minimalUserSchema, userProfileSchema } from "./users";
+import { minimalUserSchema, userIdSchema, userProfileSchema } from "./users";
+
+const sponsorTypeSchema = z.enum(["annual", "event"]);
+
+export type SponsorType = z.infer<typeof sponsorTypeSchema>;
+
+const sponsorLevelSchema = z.object({
+  name: z.string().trim(),
+  objectID: z.string().trim(),
+  order_number: z.number(),
+  type: sponsorTypeSchema,
+});
+
+export type SponsorLevel = z.infer<typeof sponsorLevelSchema>;
 
 const shared = z.object({
-  admin_uids: z.array(minimalUserSchema),
+  admin_uids: z.array(userIdSchema),
+  admins: z.array(minimalUserSchema),
   description: z.string().trim().optional(),
   industry: z.string().trim(),
   location: z.string().trim(),
@@ -14,7 +28,7 @@ const shared = z.object({
   order_number: z.number().optional(),
   size: z.string().trim().optional(),
   sponsor: z.boolean().default(false),
-  sponsor_level: z.string().default(""),
+  sponsor_level: sponsorLevelSchema,
   status: statusSchema,
   url: z.string().url(),
   visible_by: visibleBySchema,
